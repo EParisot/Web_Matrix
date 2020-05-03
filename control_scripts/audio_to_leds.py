bbbb26c86ec4345f04991d1cd1e1900c6b5cd5f1
eparisot <eparisot@student.42.fr>
2020-05-03 17:02:36 +0200

    requ

--- control_scripts/audio_to_leds.py
+++ control_scripts/audio_to_leds.py
@@ -17,30 +17,30 @@ RATE = 48000
 CHUNK = 1024
 
 async def clear():
-    async with websockets.connect(ws) as websocket:
+    async with websockets.connect(ws, ping_interval=None) as websocket:
         json_data = json.dumps([{"clear": 1}])
         await websocket.send(json_data)
 
 async def send_data(filter=False):
-    color = [0, 200, 0]
-    async with websockets.connect(ws) as websocket:
+    color = [200, 200, 0]
+    async with websockets.connect(ws, ping_interval=None, max_queue=512) as websocket:
         while True:
             audio_data = np.frombuffer(stream.read(CHUNK), dtype=DTYPE)
             norm = audio_data / MAX_INT
             fourier = np.abs(np.fft.rfft(norm))
-            fourier = np.delete(fourier, len(fourier)-1)[:64]
+            fourier = fourier[:64]
             matrix = np.reshape(fourier, (32, len(fourier)//32))
             matrix = np.int_(np.divide(np.multiply(np.mean(matrix, axis=1), 8), 150))
             cmd = [{"clear": 1}]
             for x, y in enumerate(matrix):
                 for _y in range(y):
-                    cmd.append({"pos": [x, 8 - int(_y)], "color": color})
+                    cmd.append({"pos": [x, 7 - int(_y)], "color": [color[0]*int(_y)/8, color[1]*(8 - int(_y))/8, 0]})
             json_data = json.dumps(cmd)
             await websocket.send(json_data)
             try:
                 await websocket.recv()
             except:
-                websocket = await websockets.connect(ws)
+                websocket = await websockets.connect(ws, ping_interval=None, max_queue=512)
 
 
 p=pyaudio.PyAudio() # start the PyAudio class
